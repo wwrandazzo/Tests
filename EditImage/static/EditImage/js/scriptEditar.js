27857ac1d3d2b95d964a80a26d9788c59651cc3a
WAR1977 <wwrandazzo@gmail.com>
2020-10-17 08:31:42 -0300

    Paint sobre Imagen
    Paint sobre Imagen

--- EditImage/static/EditImage/js/scriptEditar.js
+++ EditImage/static/EditImage/js/scriptEditar.js
@@ -1,10 +1,48 @@
-var imgWidth, imgHeight, datosPrim;
+var imgWidth, imgHeight, datosPrim, x=0, y=0, dibujando=false;
+
 $(document).on("ready",function(){
     var canvas = document.getElementById("miCanvas");
     $("#btnBN").on("click",function(){
         aBlancoyNegro(ctx,canvas);
         });
+    $("#btnBR").on("click",function(){
+        RestaurarImagen(ctx);
+        });
     var ctx= canvas.getContext("2d");
+    //prueba
+    var rect=canvas.getBoundingClientRect();
+    canvas.addEventListener('mousedown',function(e){
+        //alert("Aca entro a mousedown")
+        x=e.clientX-rect.left;
+        y=e.clientY-rect.top;
+        dibujando=true;
+    });
+    canvas.addEventListener('mousemove',function(e){
+        if (dibujando===true){
+            dibujar(x,y,e.clientX-rect.left,e.clientY-rect.top)
+            x=e.clientX-rect.left;
+            y=e.clientY-rect.top;
+        }
+    });
+    canvas.addEventListener('mouseup',function(e){
+        if (dibujando===true){
+            dibujar(x,y,e.clientX-rect.left,e.clientY-rect.top)
+            x=0;
+            y=0;
+            dibujando=false;
+        }
+    
+    });
+    function dibujar(x1,y1,x2,y2){
+        ctx.beginPath();
+        ctx.strokeStyle='yellow';
+        ctx.lineWidth=2;
+        ctx.moveTo(x1,y1);
+        ctx.lineTo(x2,y2);
+        ctx.stroke();
+        ctx.closePath();
+    }
+    //
     var img = new Image();
     img.src= "static/EditImage/img/JellyFish.jpg";
     img.onload = function(){
@@ -14,7 +52,7 @@ $(document).on("ready",function(){
         canvas.height=imgHeight/2;
         ctx.drawImage(this,0,0,imgWidth/2,imgHeight/2);
         var datosImagen = ctx.getImageData(0,0,imgWidth/2,imgHeight/2);
-        datosPrim = datosImagen.data;
+        datosPrim = datosImagen;
         };
     });
 function aBlancoyNegro(ctx,canvas){
@@ -24,11 +62,12 @@ function aBlancoyNegro(ctx,canvas){
         var verde = data.data[index + 1];
         var azul = data.data[index+ 2];
         var valor = (rojo + verde + azul) / 3;
-  
         data.data[index] = valor;
         data.data[index + 1] = valor;
         data.data[index + 2] = valor;
-      }
-  
-      ctx.putImageData(data, 0, 0);
     }
+    ctx.putImageData(data, 0, 0);
+}
+function RestaurarImagen(ctx){
+    ctx.putImageData(datosPrim, 0, 0);
+}
